873a5365956b9078eaf743b82ac5a37c07db9f14
Ben Smith <bensmith8229@gmail.com>
2023-09-21 17:55:32 -0500

    Added new empty README

---
+++ src/helpers.py
@@ -0,0 +1,77 @@
+import json, re
+from pathlib import Path
+
+
+class Helpers:
+    CONFIG_PATH = Path("config.cfg")
+    CACHE_PATH = Path("CACHE.cache")
+    CONFIG_STRUCT = {"logging": "verbose/vital",
+                     "log_path": "log.txt",
+                     "refresh_interval": 60 * 5,
+                     "db_uri": ""
+                     }
+    MONTH_MAP = {"January": 1,
+                "February": 2,
+                "March": 3,
+                "April": 4,
+                "May": 5,
+                "June": 6,
+                "July": 7,
+                "August": 8,
+                "September": 9,
+                "October": 10,
+                "November": 11,
+                "December": 12
+                }
+    
+    @staticmethod 
+    def convert_pastebin_date(strv):
+        matches = re.findall("\S+", strv)
+        matches.remove("of")
+        
+        month = Helpers.MONTH_MAP[matches[2]]
+        day = re.match(r'([0-9]+)', matches[1]).groups()[0]
+        year = matches[3]
+        stamp = " ".join(matches[4:7])
+        
+        return f"{month}/{day}/{year} {stamp}"
+
+    @staticmethod
+    def validate_config():
+        if not Path.exists(Helpers.CONFIG_PATH):
+            with open(Helpers.CONFIG_PATH, "w") as config_file:
+                config_file.write(json.dumps(Helpers.CONFIG_STRUCT, indent=4))
+            raise Exception("Unconfigured config file.")
+
+        with open(Helpers.CONFIG_PATH, "r") as config:
+            contents = json.load(config)
+
+        if contents == Helpers.CONFIG_STRUCT:
+            raise Exception("Unconfigured config file.")
+
+        return contents
+
+    @staticmethod
+    def get_cache():
+        if not Path.exists(Helpers.CACHE_PATH):
+            with open(Helpers.CACHE_PATH, "w") as cache_file:
+                    json.dump({"cache": []}, cache_file)
+
+        with open(Helpers.CACHE_PATH, "r") as cache_file:
+            return json.load(cache_file)
+        
+    @staticmethod
+    def append_cache(id):
+        cache = Helpers.get_cache()["cache"]
+        if id in cache:
+            return
+        elif len(cache) > 50:
+            cache.pop(0)
+        
+        cache.append(id)
+        Helpers.dump_cache(cache)
+    
+    @staticmethod
+    def dump_cache(obj):
+        with open(Helpers.CACHE_PATH, "w") as cache_file:
+            json.dump(obj, cache_file)
